5b5ad6635404702da7484c0b40ffa04d42dcc8c3
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-05 13:21:52 +0200

    .

---
+++ static/hatv.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import cmath
+
+fb = 101
+x = st.slider("Kitevő", min_value=0.0, max_value=5.0, value=0.0, step=0.01)
+n = complex(x, 0)
+
+"""
+$$f(x) = x^{%s}$$
+""" % round(
+    x, 2
+)
+
+lr = list(
+    map(
+        lambda x: (complex(x[0] - ((fb - 1) // 2), 0) ** n).real,
+        enumerate([0] * fb),
+    )
+)
+
+lc = list(
+    map(
+        lambda x: (complex(x[0] - ((fb - 1) // 2), 0) ** n).imag,
+        enumerate([0] * fb),
+    )
+)
+
+k = list(map(lambda x: x[0] - (fb - 1) / 2, enumerate([0] * fb)))
+
+chart_data = pd.DataFrame({"x": k, "real": lr, "imag": lc})
+
+st.line_chart(chart_data, x="x", y=["real", "imag"])
