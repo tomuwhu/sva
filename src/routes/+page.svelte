7a09528bc388cafbda359b4236d4e3be16de6c9d
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-05 10:25:58 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,19 @@
 
 <h1>Ságváris oktatás</h1>
 
+<br /><br />
 <a href="{base}/hatv">Hatványozás</a>
-<br>
-<br>
-<a href="https://github.com/tomuwhu/sva"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></a>
+<br />
+<br />
+<br /><br />
+<a href="https://github.com/tomuwhu/sva"
+	><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" /></a
+>
+
+<style>
+	img {
+		width: 60px;
+		box-shadow: 1px 1px 4px black;
+		border-radius: 30px;
+	}
+</style>
