f0d76ac515fd4331ad86397d463cd67a6f2aebd3
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-05 20:13:07 +0200

    .

--- static/hatv.py
+++ static/hatv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import cmath
 
-fb = 101
+fb = 51
 x = st.slider("Kitevő", min_value=0.0, max_value=5.0, value=0.0, step=0.01)
 n = complex(x, 0)
 
@@ -31,4 +31,4 @@ k = list(map(lambda x: x[0] - (fb - 1) / 2, enumerate([0] * fb)))
 
 chart_data = pd.DataFrame({"x": k, "real": lr, "imag": lc})
 
-st.line_chart(chart_data, x="x", y=["real", "imag"])
+st.line_chart(chart_data, x="x", y=["imag", "real"])
